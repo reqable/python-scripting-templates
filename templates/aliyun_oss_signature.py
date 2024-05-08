42a9796d2796b0643aa54116d7089fb6f4783d27
MegatronKing <1256980529@qq.com>
2024-05-08 12:18:56 +0800

    Opt aliyun_oss_signature.py

--- templates/aliyun_oss_signature.py
+++ templates/aliyun_oss_signature.py
@@ -4,9 +4,11 @@ import hashlib
 import base64
 import datetime
 
+# TODO Replace with your keys
+access_key_id = 'Your key id'
+access_key_secret = 'Your key secret'
+
 def onRequest(context, request):
-  access_key_id = 'Your key id'
-  access_key_secret = 'Your key secret'
   date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
   signature_string = f"GET\n\n\n{date}\n"
   signature_string += f"/{context.host.split('.')[0]}{request.path}"
